8a01c355a321a52b33e357d61d50dc4e0ed33f3a
Ana Nikolova <ani.nik19@gmail.com>
2021-04-22 17:49:26 +0200

    routing done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import{BrowserRouter as Router,Switch,Route} from "react-router-dom"
+import ProductListing from "./containers/ProductListing"
+import ProductDetail from "./containers/ProductDetail"
+import Header from "./containers/Header"
 
 function App() {
   return (
     <h1 className="App">
-      Hello
+      <Router>
+        <Header/>
+        <Switch>
+        <Route path="/" exact component={ProductListing}/>
+        <Route path="/product/:product" exact  component={ProductDetail}/> 
+        <Router>404 Not Found</Router>
+        </Switch>
+      </Router>
     </h1>
   );
 }      
---
+++ src/containers/ProductListing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProductListing = () => {
+    return (
+        <div>
+            <h1>
+                Productlisting 
+            </h1>
+        </div>
+    )
+}
+
+export default ProductListing
+
